ec524003329d22e3758e2ab0af34711fd2d8e868
Andrey Soroko <andreysoroko84@gmail.com>
2023-02-27 09:44:09 +0300

    feat: add quotes

--- scripts/quotes.js
+++ scripts/quotes.js
@@ -11,9 +11,9 @@ async function getQuotes() {
   const quotes = './assets/quotes/data.json';
   const res = await fetch(quotes);
   const data = await res.json();
-  console.log(data);
+  // console.log(data);
   randomQuote =   getRandomInt(data.length);
-  console.log(randomQuote);
+  // console.log(randomQuote);
 
   author.textContent = data[randomQuote].author;
   quote.textContent = data[randomQuote].text;
